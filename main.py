76166dc6840ad80c8304c0811c9e35b2cb0bb8f2
Andrey Stekachev <zerov.live@gmail.com>
2023-12-11 17:48:21 +0500

    gpt changes

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ bot = telebot.TeleBot(TgKey)
 
 
 @bot.message_handler(regexp = '/choose')
-def welcome(message):
+def welcomeMessage(message):
     bot.send_message(message.chat.id , "Привет")
 
 
 @bot.message_handler(content_types=["text"])
 def talk(message):
     response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message.text,
-        temperature=0.2,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.5
+        model = "text-davinci-003",
+        prompt = message.text,
+        temperature = 0.2,
+        max_tokens = 1000,
+        top_p = 1.0,
+        frequency_penalty = 0.5,
+        presence_penalty = 0.5
     )
     gpt_text = response["choices"][0]["text"]
     bot.send_message(message.chat.id, gpt_text)
